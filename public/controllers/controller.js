30870712ae1f4de09811602ca4a5b99a20e36d16
Toan <toan@wontoan.com>
2015-09-12 18:10:57 -0400

    User can add a contact to database
    -Add remove button to site layout
    -Implement POST request

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -2,8 +2,20 @@ var myApp = angular.module('myApp', []);
 
 myApp.controller('AppController', ['$scope', '$http', function($scope, $http) {
 
-  $http.get('/contactlist').success(function(response){
-    $scope.contactlist = response;
-  });
+  var refresh = function() {
+    $http.get('/contactlist').success(function(response){
+      $scope.contactlist = response;
+      $scope.contact = "";
+    });
+  };
+
+  refresh();
+
+  $scope.addContact = function() {
+    $http.post('/contactlist', $scope.contact).success(function(response){
+      console.log(response);
+      refresh();
+    });
+  };
 
 }]);
--- server.js
+++ server.js
@@ -2,13 +2,21 @@ var express = require ('express');
 var app = express();
 var mongojs = require ('mongojs');
 var db = mongojs('contactlist', ['contactlist']);
+var bodyParser = require('body-parser');
 
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
 
 app.get('/contactlist', function (req, res){
   db.contactlist.find(function(err, docs) {
     console.log(docs);
     res.json(docs);
+  });
+});
+
+app.post('/contactlist', function(req, res){
+  db.contactlist.insert(req.body, function(err, doc) {
+    res.json(doc);
   });
 });
 
